1f428f4e88f3aa787576ada6b4bd1d955ed0a728
Belal Khan <probelalkhan@gmail.com>
2016-01-10 12:20:27 +0530

    Initial commit

---
+++ app/src/main/java/net/simplifiedcoding/androidcustomgridview/MainActivity.java
@@ -0,0 +1,104 @@
+package net.simplifiedcoding.androidcustomgridview;
+
+import android.app.ProgressDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    //Web api url
+    public static final String DATA_URL = "http://www.simplifiedcodingreaders.16mb.com/superheroes.php";
+
+    //Tag values to read from json
+    public static final String TAG_IMAGE_URL = "image";
+    public static final String TAG_NAME = "name";
+
+    //GridView Object
+    private GridView gridView;
+
+    //ArrayList for Storing image urls and titles
+    private ArrayList<String> images;
+    private ArrayList<String> names;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        gridView = (GridView) findViewById(R.id.gridView);
+
+        images = new ArrayList<>();
+        names = new ArrayList<>();
+
+        //Calling the getData method
+        getData();
+    }
+
+    private void getData(){
+        //Showing a progress dialog while our app fetches the data from url
+        final ProgressDialog loading = ProgressDialog.show(this, "Please wait...","Fetching data...",false,false);
+
+        //Creating a json array request to get the json from our api
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(DATA_URL,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        //Dismissing the progressdialog on response
+                        loading.dismiss();
+
+                        //Displaying our grid
+                        showGrid(response);
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }
+        );
+
+        //Creating a request queue
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        //Adding our request to the queue
+        requestQueue.add(jsonArrayRequest);
+    }
+
+
+    private void showGrid(JSONArray jsonArray){
+        //Looping through all the elements of json array
+        for(int i = 0; i<jsonArray.length(); i++){
+            //Creating a json object of the current index
+            JSONObject obj = null;
+            try {
+                //getting json object from current index
+                obj = jsonArray.getJSONObject(i);
+
+                //getting image url and title from json object
+                images.add(obj.getString(TAG_IMAGE_URL));
+                names.add(obj.getString(TAG_NAME));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        //Creating GridViewAdapter Object
+        GridViewAdapter gridViewAdapter = new GridViewAdapter(this,images,names);
+
+        //Adding adapter to gridview
+        gridView.setAdapter(gridViewAdapter);
+    }
+
+}
